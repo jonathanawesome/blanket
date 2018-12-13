14ace3c88f1b306b9248de5a2fb06c9d3e297edb
jonathan brennan <jon@safetyjon.com>
2018-12-13 16:36:38 -0600

    updated eslint/prettier config

--- src/scripts/something.js
+++ src/scripts/something.js
@@ -1,17 +1,15 @@
 export const something = (() => {
-
   const body = document.body;
 
   const init = () => {
     //initialize
-  }
+  };
 
   const _getSomething = () => {
     //private
-  }
+  };
 
   return {
     init: init,
   };
-
 })();
